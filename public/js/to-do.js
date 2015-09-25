a42437069a84ab931e19a17abb57a2527a77e1d8
Isabella Organ <isabellaorgan@gmail.com>
2015-09-25 11:59:29 -0700

    clean up tabs etc

--- public/js/to-do.js
+++ public/js/to-do.js
@@ -7,22 +7,19 @@ $(document).ready(function () {
   function enter_task() {
     var text = $('#enter_task').val();
     $('#todo_list').append('<li><span>' + text + ' </span><input type="submit" id="edit' + counter + '" value="Edit">' + '<input type="submit" class="done" id="delete' + counter + '" value="Delete">' + '</li>');
-
-$('#edit' + counter).click(function () {
-  $(this).prev().attr('contenteditable', 'true');
-  $(this).prev().focus();
+    $('#edit' + counter).click(function () {
+      $(this).prev().attr('contenteditable', 'true');
+      $(this).prev().focus();
 });
+    $('#delete' + counter).click(function () {
+      $(this).parent().remove();
+    });
 
-$('#delete' + counter).click(function () {
-  $(this).parent().remove();
-});
+    counter++;
+  }
 
-  counter++;
-}
-
-$(function() {
-  $('#add_task').on('click', enter_task);
-});
-
+  $(function() {
+    $('#add_task').on('click', enter_task);
+  });
 
 });
